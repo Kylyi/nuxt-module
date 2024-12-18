81e650f5c445133b4ce0a17ddbfc3195bbf3dda7
Kylyi <jacob.kylian@gmail.com>
2024-12-18 15:39:08 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from "@nuxt/kit"
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -9,5 +12,6 @@ export default defineNuxtConfig({
     dirs: [
       { path: './components', pathPrefix: false }
     ]
-  }
+  },
+  alias: { "@layer-one": resolve("./") },
 })
